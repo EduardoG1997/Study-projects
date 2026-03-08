d43829f9b7a367b2bb9298fc6bf79dd986bb1826
EduardoG1997 <151768099+EduardoG1997@users.noreply.github.com>
2026-03-08 14:51:37 -0600

    Add files via upload
    An exercise to practice loops

---
+++ Loops project 4 study #1.py
@@ -0,0 +1,60 @@
+# You are the Data Analyst at Carly’s Clippers, the newest hair salon on the block. 
+# Your job is to go through the lists of data that have been collected in the past couple of weeks. 
+# You will be calculating some important metrics that Carly can use to plan out 
+# the operation of the business for the rest of the month. THIS IS FOR PRACTICE LOOPS 
+
+
+hairstyles = ["bouffant", "pixie", "dreadlocks", "crew", "bowl", "bob", "mohawk", "flattop"]
+
+prices = [30, 25, 40, 20, 20, 35, 50, 35]
+
+last_week = [2, 3, 5, 8, 4, 4, 6, 2]
+
+# Carly wants to be able to market her low prices. We want to find out what the average price of a cut is.
+# First, let’s sum up all the prices of haircuts. Create a variable total_price, and set it to 0.
+
+total_price = 0
+
+# Loop through the prices list and add each price to the variable total_price.
+
+for price in prices:
+  total_price += price
+
+# After your loop, create a variable called average_price that is the total_price divided by the number of prices.
+# You can get the number of prices by using the len() function. Print the value of average_price so the output looks like:
+#   Average Haircut Price: <average_price>
+
+average_price = total_price / len(prices)
+print("Average Haircut Price: ${0}".format(average_price))
+
+# That average price is more expensive than Carly thought it would be! She wants to cut all prices by 5 dollars.
+# Use a list comprehension to make a list called new_prices, which has each element in prices minus 5. / Print new_prices.
+
+new_prices = [price - 5 for price in prices]
+print(new_prices)
+
+# Carly really wants to make sure that Carly’s Clippers is a profitable endeavor. She first wants to know how much revenue was brought in last week.
+# Create a variable called total_revenue and set it to 0.  Use a for loop to create a variable i that goes from 0 to len(hairstyles) 
+# Add the product of prices[i] (the price of the haircut at position i) and last_week[i] (the number of people who got 
+# the haircut at position i) to total_revenue at each step.
+#After your loop, print the value of total_revenue, so the output looks like: Total Revenue: <total_revenue
+
+total_revenue = 0
+for i in range(len(hairstyles)):
+  total_revenue += prices[i] * last_week[i]
+print("Total Revenue: ${0}".format(total_revenue))
+
+#Find the average daily revenue by dividing total_revenue by 7. Call this number average_daily_revenue and print it out.
+#Carly thinks she can bring in more customers by advertising all of the haircuts she has that are under 30 dollars.
+#Use a list comprehension to create a list called cuts_under_30 that has the entry hairstyles[i] 
+#for each i for which new_prices[i] is less than 30.
+#You can use range(len(new_prices)) in your list comprehension to make i go from 0 to the last index of new_prices.
+
+#Print cuts_under_30. 
+
+average_dialy_revenue = total_revenue / 7 
+cuts_under_30 = [hairstyles[i] for i in range(len(hairstyles)) if new_prices[i] < 30]
+
+ 
+
+    # CONGRATS YOU MADE IT! 
